1a67fdf656bf2bcbc7d994daf477447f542fb931
Yaxin Cheng <watashij@users.noreply.github.com>
2016-02-06 20:53:43 +0000

    Fixed almost all the format problems. 	--Emily, Yaxin

--- src/com/example/library/backend/BookService.java
+++ src/com/example/library/backend/BookService.java
@@ -66,7 +66,7 @@ public class BookService {
         Collections.sort(arrayList, new Comparator<Book>() {
             @Override
             public int compare(Book o1, Book o2) {
-                return (o1.getTitle().compareTo(o2.getTitle()));
+                return (o1.compareTo(o2));
             }
         });
         
