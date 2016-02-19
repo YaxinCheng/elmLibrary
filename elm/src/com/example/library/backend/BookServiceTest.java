8fbe78727ac32aa2360ff24d1be3e84e67dd3df7
ycheng <ycheng@abf6aec0-f856-415d-9ed1-abdb82332272>
2016-02-19 17:57:56 +0000

    BookServiceTest finished.	--Yaxin, Salman

--- elm/src/com/example/library/backend/BookServiceTest.java
+++ elm/src/com/example/library/backend/BookServiceTest.java
@@ -2,8 +2,15 @@ package com.example.library.backend;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
+
+import com.vaadin.data.Container.Filter;
+import com.vaadin.data.util.filter.Compare;
 
 public class BookServiceTest {
 	
@@ -28,6 +35,22 @@ public class BookServiceTest {
 	
 	@Test
 	public void testDuplicate() {
-		
+		List<String> authors = new ArrayList(Arrays.asList("T"));
+		Object id = BookService.shelf.addEntity(new Book("123", "1",authors, "1","2016"));
+		assertTrue(BookService.checkDuplicate("123"));
+		instance.delete(BookService.shelf.getItem(id));
+	}
+	
+	@Test
+	public void testRemoveFilters() {
+		List<String> authors = new ArrayList(Arrays.asList("T"));
+		Object id = BookService.shelf.addEntity(new Book("123", "1",authors, "1","2016"));
+		assertTrue(instance.count() > 0);
+		Filter filter = new Compare.Equal("isbn", "");
+		instance.shelf.addContainerFilter(filter);
+		assertTrue(instance.count() == 0);
+		instance.removeAllFilters();
+		assertTrue(instance.count() > 0);
+		instance.delete(BookService.shelf.getItem(id));
 	}
 }
