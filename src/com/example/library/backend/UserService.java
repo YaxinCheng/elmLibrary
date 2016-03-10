ab7a38615e67e58660a0732a5cf6424e0fd49515
macdona5 <macdona5@abf6aec0-f856-415d-9ed1-abdb82332272>
2016-03-10 22:21:57 +0000

    Removed code related to borrowed, waiting/held books from UserService [Nick, Emily]

--- src/com/example/library/backend/UserService.java
+++ src/com/example/library/backend/UserService.java
@@ -21,6 +21,7 @@ public class UserService {
 	public static UserService createDemoService() {
 		if (instance == null) {
 			BookService BookService = new BookService();
+			BookService.createDemoService();
 			UserService UserService = new UserService();
 			instance = UserService;
 			UserService.populateUserService(BookService);
@@ -32,33 +33,20 @@ public class UserService {
 	public void populateUserService(BookService b){
 		List<Book> borrowed = null;
 		List<Book> waiting = null;
-		
-		borrowed.add((Book) b.shelf.getItem("B012OUZ3EQ"));
-		borrowed.add((Book) b.shelf.getItem("11783986549"));
 		User user1 = new User("Nick", "definitelynot@fake.ca", "902-666-1234", borrowed, waiting);
 		user1.setAccount("nick");
 		user1.setPassword("testlol!!");
 		Users.addEntity(user1);
-		borrowed.clear();
-		waiting.clear();
-		
-		borrowed.add((Book) b.shelf.getItem("20293429872"));
-		waiting.add((Book) b.shelf.getItem("11783986549"));
-		waiting.add((Book) b.shelf.getItem("42039487621"));
+
 		User user2 = new User("Roc", "prince@god.org", "902-123-6969", borrowed, waiting);
 		user2.setAccount("roc");
 		user2.setPassword("poutine4@");
 		Users.addEntity(user2);
-		borrowed.clear();
-		waiting.clear();
-		
-		waiting.add((Book) b.shelf.getItem("9823742983"));
+
 		User user3 = new User("Prince", "real@email.com", "666-123-4567", borrowed, waiting);
 		user3.setAccount("prince90");
 		user3.setPassword("securepassword!");
 		Users.addEntity(user3);
-		borrowed.clear();
-		waiting.clear();
 	}
 	
 	public synchronized int count() {
