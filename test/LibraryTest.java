e0db76aab702bdd5742b3652808e1bc3288f57a4
ziw <ziw@abf6aec0-f856-415d-9ed1-abdb82332272>
2016-03-26 16:53:15 +0000

    Finish the testBench for login page, but the result is still unstable -Roc

--- test/LibraryTest.java
+++ test/LibraryTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -44,7 +45,7 @@ public class LibraryTest extends TestBenchTestCase {
 	 * @throws Exception
 	 */
 	@Test
-	public void testLogIn() throws Exception {
+	public void test1() throws Exception {
 		openTestUrl();
 		assertTrue($(TextFieldElement.class).exists());
 		assertTrue($(PasswordFieldElement.class).exists());
@@ -54,21 +55,14 @@ public class LibraryTest extends TestBenchTestCase {
 		ButtonElement LoginButton = $(ButtonElement.class).caption("Log In").first();
 		LoginButton.click();
 		// There should now be one notification with the specified text
-		assertEquals("Username and account do not match.", $(NotificationElement.class).first().getText());
+		assertEquals("Username and account do not match.", $(NotificationElement.class).first().getText().toString());
 	}
 
+	/*
+	 * Test the Cancel Button
+	 */
 	@Test
-	public void testRegister() throws Exception {
-		openTestUrl();
-		ButtonElement RegisterButton = $(ButtonElement.class).caption("Register").first();
-		RegisterButton.click();
-		ButtonElement SaveButton = $(ButtonElement.class).caption("Save").first();
-		SaveButton.click();
-		assertEquals("Please enter the correct name format", $(NotificationElement.class).first().getText());
-	}
-
-	@Test
-	public void testCancel() throws Exception {
+	public void test2() throws Exception {
 		openTestUrl();
 		ButtonElement RegisterButton = $(ButtonElement.class).caption("Register").first();
 		RegisterButton.click();
@@ -79,17 +73,24 @@ public class LibraryTest extends TestBenchTestCase {
 		assertEquals(2, $(ButtonElement.class).all().size());
 	}
 
+	/*
+	 * Test Save Button after click Register button
+	 */
 	@Test
-	public void LogInSucc() throws Exception {
+	public void test3() throws Exception {
 		openTestUrl();
-		TextFieldElement user = $(TextFieldElement.class).id("6");
-		user.setValue("RocKing");
-		PasswordFieldElement Pass = $(PasswordFieldElement.class).id("7");
-		Pass.setValue("Wz!23456");
+		ButtonElement RegisterButton = $(ButtonElement.class).caption("Register").first();
+		RegisterButton.click();
+		ButtonElement SaveButton = $(ButtonElement.class).caption("Save").first();
+		SaveButton.click();
+		assertEquals("Please enter the correct name format", $(NotificationElement.class).first().getText().toString());
 	}
 
+	/*
+	 * Test Register Successfully
+	 */
 	@Test
-	public void RegisterSucc() throws Exception {
+	public void test4() throws Exception {
 		openTestUrl();
 		ButtonElement RegisterButton = $(ButtonElement.class).caption("Register").first();
 		RegisterButton.click();
@@ -105,7 +106,47 @@ public class LibraryTest extends TestBenchTestCase {
 		phone.setValue("9021234567");
 		ButtonElement SaveButton = $(ButtonElement.class).caption("Save").first();
 		SaveButton.click();
-		// assertEquals(1, $(NotificationElement.class).all().size());
 		assertEquals("Successfully registered.", $(NotificationElement.class).first().getText());
+	}
+
+	/*
+	 * Test Register Failed with the same information before
+	 */
+	@Test
+	public void test5() throws Exception {
+		openTestUrl();
+		ButtonElement RegisterButton = $(ButtonElement.class).caption("Register").first();
+		RegisterButton.click();
+		TextFieldElement account = $(TextFieldElement.class).id("1");
+		account.setValue("RocKing");
+		PasswordFieldElement password = $(PasswordFieldElement.class).id("2");
+		password.setValue("Wz!23456");
+		TextFieldElement name = $(TextFieldElement.class).id("3");
+		name.setValue("Roc");
+		TextFieldElement email = $(TextFieldElement.class).id("4");
+		email.setValue("123@234.com");
+		TextFieldElement phone = $(TextFieldElement.class).id("5");
+		phone.setValue("9021234567");
+		ButtonElement SaveButton = $(ButtonElement.class).caption("Save").first();
+		SaveButton.click();
+		assertEquals("The account is already in the database.",
+				$(NotificationElement.class).first().getText().toString());
+	}
+
+	/*
+	 * Test Log In Successfully
+	 */
+	@Test
+	public void test6() throws Exception {
+		openTestUrl();
+		TextFieldElement user = $(TextFieldElement.class).id("6");
+		user.setValue("RocKing");
+		PasswordFieldElement Pass = $(PasswordFieldElement.class).id("7");
+		Pass.setValue("Wz!23456");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
 	}
 }
